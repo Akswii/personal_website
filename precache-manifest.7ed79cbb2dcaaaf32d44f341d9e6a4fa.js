2e42d926fc58d0e4a23befb93db406902ed9a2e9
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-01-25 13:49:02 +0100

    Updates

---
+++ precache-manifest.7ed79cbb2dcaaaf32d44f341d9e6a4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c7aec797eb8d07f3c69",
+    "url": "/static/js/main.6c7aec79.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6c7aec797eb8d07f3c69",
+    "url": "/static/css/main.e01f6522.chunk.css"
+  },
+  {
+    "revision": "8148cd5a006cb761e24c2daa6fcd4e35",
+    "url": "/index.html"
+  }
+];
